0b3e2f8cae4a8b5dc61821a7b20acacbda5b36e3
raul.muscaliuc <raul.muscaliuc@dialogdata.de>
2020-10-28 21:51:16 +0200

    Tema5

---
+++ Raul-Andrei-MUSCALIUC/L05/Question.Domain/CreateQuestionWorkflow/CreateQuestionCmd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    class CreateQuestionCmd
+    {
+        [Required]
+        public string Title { get; private set; }
+        [Required]
+        public string Description { get; private set; }
+        [Required]
+        public string[] Tags { get; private set; }
+        [Required]
+        public int Votes { get; private set; }
+
+        public CreateQuestionCmd(string Title, string Description, string Tags, int Votes)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.Tags = Tags;
+            this.Votes = Votes;
+        }
+    }
+}
---
+++ Raul-Andrei-MUSCALIUC/L05/Question.Domain/CreateQuestionWorkflow/InvalidTagsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Question.Domain.CreateQuestionWorkflow
+{
+    public class InvalidTagsException : Exception
+    {
+        public InvalidTagsException(int numberOfTags) : base($"The value \"{numberOfTags}\" is an invalid tags number.")
+        {
+        }
+    }
+}
